01b09ba03a9dbdbcb3046c624f031bda6dc139d4
Cameron Dugan <cameron.dugan@protonmail.com>
2025-09-26 22:58:27 -0400

    fix: flake and ci changes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "CameronDugan.com utility flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+          cargo
+        ];
+        shellHook = ''
+          cargo install mdbook
+          cargo install mdbook-rss
+          cargo install mdbook-external-links
+          cargo install mdbook-linkcheck
+          cargo install mdbook-reading-time
+          cargo install mdbook-tiny
+        '';
+      };
+    }
+  );
+}
